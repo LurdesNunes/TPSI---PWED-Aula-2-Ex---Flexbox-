f4ab90f73fa965c0dc3a21b7615b06dff2eda4a9
Lurdes Nunes <lurdesmanunes2002@outlook.pt>
2022-10-11 17:30:27 +0100

    justifym align e banners

--- js/flex.js
+++ js/flex.js
@@ -4,3 +4,21 @@ function flex(valor){
 function wrap(valor){
     document.getElementById('container').style.flexWrap=valor;
 }
+function align(valor){
+    document.getElementById('container').style.alignItems=valor;
+}
+function slide(){
+    document.getElementById('publicidade').src="../html/email.png";
+    document.getElementById('descricao').innerHTML="email";
+    setTimeout("slide2()", 2000)
+}
+function slide2(){
+    document.getElementById('publicidade').src="../html/imc.png";
+    document.getElementById('descricao').innerHTML="imc";
+    setTimeout("slide3()", 2000)
+}
+function slide3(){
+    document.getElementById('publicidade').src="../html/isla.png";
+    document.getElementById('descricao').innerHTML="isla";
+    setTimeout("slide()", 2000)
+}
